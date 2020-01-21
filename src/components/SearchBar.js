a6a7f579d700175e7b986901e846abd73ed5f8e8
sumit kumar jha <sksumitkumar003@gmail.com>
2020-01-21 18:24:44 +0530

    created Searchbar and App component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class SearchBar extends React.Component{
+
+    render(){
+        return(
+            <h1> SearchBar</h1>
+        )
+    }
+}
+
+export default SearchBar

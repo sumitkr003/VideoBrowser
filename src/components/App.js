8b1f2ff5671b776637190a43cc4a43f56b70bc12
sumit kumar jha <sksumitkumar003@gmail.com>
2020-01-21 20:17:19 +0530

    integrated youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import youtube from '../apis/youtube' 
 
 class App extends React.Component{
 
+    constructor(props){
+        super(props)
+        this.state = {
+            videos : []
+        }
+    }
+
+    SearchYoutube = async (search_text) => {
+        const response = await youtube.get('/search', {
+            params : {
+                q : search_text
+            }
+        })
+
+        this.setState({videos : response.data.items})
+    }
+
     render(){
         return(
-            <div>
-                <SearchBar />
+            <div className="ui container">
+                <SearchBar onFormSubmit = {this.SearchYoutube} />
+                <p> I have {this.state.videos.length} videos</p>
             </div>
         )
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,9 +2,32 @@ import React from 'react'
 
 class SearchBar extends React.Component{
 
+    constructor(props){
+        super(props)
+        this.state = {
+            search_text : ''
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({search_text : event.target.value })
+    }
+
+    submitForm = (event) => {
+        event.preventDefault()
+        this.props.onFormSubmit(this.state.search_text);
+    }
+
     render(){
         return(
-            <h1> SearchBar</h1>
+            <div className="search-bar ui segment">
+                <form onSubmit={this.submitForm} className="ui form">
+                    <div className="field">
+                        <label> <h3>Video Search</h3></label>
+                        <input type="text" value={this.state.search_text} onChange={this.handleChange} />
+                    </div>
+                </form>
+            </div>
         )
     }
 }
